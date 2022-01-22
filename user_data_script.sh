eefba701b93b91cd1629f8d26429b11e9fe8aa6a
jkeane889 <jkeane889@gmail.com>
2022-01-21 19:16:30 -0600

    changed main file for app

--- user_data_script.sh
+++ user_data_script.sh
@@ -25,4 +25,4 @@ sudo chmod -R 755 .
 #install node module
 npm install
 # start the app
-node index.js > app.out.log 2> app.err.log < /dev/null &
+node app.js > app.out.log 2> app.err.log < /dev/null &
