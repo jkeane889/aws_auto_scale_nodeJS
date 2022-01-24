e86c697892ab4182556a924a3e08479372c7add0
jkeane889 <jkeane889@gmail.com>
2022-01-24 10:33:56 -0600

    added redis adapter

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
+import { createClient } from "redis";
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+import { createAdapter } from "@socket.io/redis-adapter";
+
+// config for socket.io
+const config = {
+    pingTimeout: 60000,
+    pingInterval: 25000,
+    transports: ['websocket'],
+};
 
 const app = express();
-
-const port = 3000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, config);
 
 app.get('/', (req, res) => {
     res.send('<h1>Auto Scaling App</h1><h4>Message: Success!</h4><p>Version: 1.0.0</p>')
 })
 
-app.listen(port, () =>{
-    console.log(`Demo app is up and listening to port ${port}`)
-})
+// TODO: save Redis cluster host name in environmental file
+const pubClient = createClient({ host: 'redis-cluster-test.sufcdc.clustercfg.use1.cache.amazonaws.com', port: 6379 });
+const subClient = pubClient.duplicate();
 
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+    io.adapter(createAdapter(pubClient, subClient));
+});
+
+io.on("connection", (socket) => {
+    console.log('New user connected')
+});
+
+httpServer.listen(3000);
